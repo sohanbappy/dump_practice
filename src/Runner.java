f2358aee75e3ee6ba12cd820b21267dfa2bd5c2d
sohan.molla <sohan@bracits.com>
2023-10-16 15:21:40 +0600

    Commented again

--- src/Runner.java
+++ src/Runner.java
@@ -4,9 +4,10 @@ class Runner {
 
 
     public static void main(String[] args) {
+        //
         long targetId = 8313811347991371218L;
         long timeStamp = getTimestamp(targetId);
-        //Added by BAPPY
+        //Added by 
         System.out.println("Timestamp: "+timeStamp);
         System.out.println("Diff: "+(targetId-timeStamp));
     }
